70d7896b23c825590252eb292956fca91b65074e
harshalkathar <67226285+harshalkathar@users.noreply.github.com>
2023-03-22 19:46:50 +0530

    Update main.tf

--- IAM/User-Creation/main.tf
+++ IAM/User-Creation/main.tf
@@ -15,10 +15,25 @@ output "password" {
 }
 
 
-##policy attached to user
+## user managed policy attached to user
 
 resource "aws_iam_policy_attachment" "test-attach" {
   name       = "test-attachment"
   users      = [aws_iam_user.newuser.name]
   policy_arn = aws_iam_policy.ec2fullaccess.arn
 }
+
+
+###aws managed policy attached to user
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  users      = [aws_iam_user.newuser.name]
+  for_each = toset([
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  ])
+  policy_arn = each.value
+}
+
+
