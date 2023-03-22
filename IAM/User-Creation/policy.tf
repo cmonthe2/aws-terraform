6ae4d5868db3f51e2a11b517a64b96ae9a07a831
harshalkathar <67226285+harshalkathar@users.noreply.github.com>
2023-03-22 19:23:43 +0530

    Create policy.tf

---
+++ IAM/User-Creation/policy.tf
@@ -0,0 +1,48 @@
+resource "aws_iam_policy" "ec2fullaccess" {
+  name        = "ec2fullaccess-policy"
+  description = "ec2fullaccess"
+
+  policy = jsonencode(
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Effect": "Allow",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "elasticloadbalancing:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "cloudwatch:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "autoscaling:*",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "iam:CreateServiceLinkedRole",
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "iam:AWSServiceName": [
+                        "autoscaling.amazonaws.com",
+                        "ec2scheduled.amazonaws.com",
+                        "elasticloadbalancing.amazonaws.com",
+                        "spot.amazonaws.com",
+                        "spotfleet.amazonaws.com",
+                        "transitgateway.amazonaws.com"
+                    ]
+                }
+            }
+        }
+    ]
+})
+}
